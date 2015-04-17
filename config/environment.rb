2717f807033f393a9711ac94132f9b5d7cd0c673
Shawn McDermott <shawnmcdermott@collectivebias.com>
2015-04-17 08:42:13 -0500

    config heroku redis

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 PlanningPoker::Application.initialize!
+config.gem 'redis'
+ENV["REDISTOGO_URL"] =  'redis://redistogo:1584cd3be2bf39a6e9310cd5de5e5be8@grouper.redistogo.com:10625/'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150406174200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "developers", force: true do |t|
     t.string   "name"
     t.integer  "vote"
