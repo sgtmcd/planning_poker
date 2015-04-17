d52875f4b73d7bc6de61b5fa42b56a56f9114d45
Shawn McDermott <shawnmcdermott@collectivebias.com>
2015-04-17 08:44:10 -0500

    config heroku redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ PlanningPoker::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.gem 'redis'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
